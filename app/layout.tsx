582f08fdbf9e4d00c851c7d743f4e2bd5958efdf
fabianhug <fabian@stakingrewards.com>
2024-10-21 23:15:25 +0200

    add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
-import './global.css';
-import { RootProvider } from 'fumadocs-ui/provider';
-import { Inter } from 'next/font/google';
-import type { ReactNode } from 'react';
+import "./global.css";
+import { RootProvider } from "fumadocs-ui/provider";
+import { Inter } from "next/font/google";
+import type { ReactNode } from "react";
+import { Footer } from "@/components/footer";
 
 const inter = Inter({
-  subsets: ['latin'],
+  subsets: ["latin"],
+  variable: "--font-sans",
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
@@ -12,6 +14,7 @@ export default function Layout({ children }: { children: ReactNode }) {
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body>
         <RootProvider>{children}</RootProvider>
+        <Footer />
       </body>
     </html>
   );
