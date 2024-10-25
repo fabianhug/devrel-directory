3bbb5a1faec4c16554154ccae5c0c072dd574a72
fabianhug <fabian@stakingrewards.com>
2024-10-25 23:10:21 +0200

    add launch banner

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { RootProvider } from "fumadocs-ui/provider";
 import { Inter } from "next/font/google";
 import type { ReactNode } from "react";
 import { Footer } from "@/components/footer";
+import { Banner } from "fumadocs-ui/components/banner";
+import Link from "next/link";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -13,6 +15,14 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body>
+        <Banner variant="rainbow">
+          <p>
+            We're thrilled to launch devrel.directory - read more about it{" "}
+            <Link href="/blog/2024-10-25-what-is-the-devrel-directory-and-how-to-contribute">
+              here
+            </Link>
+          </p>
+        </Banner>
         <RootProvider>{children}</RootProvider>
         <Footer />
       </body>
